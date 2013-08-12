553a5074f2747d3d48a43c06aa75bd222d9ad76c
Damon Oehlman <damon.oehlman@gmail.com>
2013-08-12 22:12:50 +1000

    Implemented one-to-one messaging tests

--- one-to-one.js
+++ one-to-one.js
@@ -2,4 +2,44 @@ var test = require('tape');
 
 module.exports = function(creator) {
   var messengers = [creator(), creator()];
+
+  test('0 --> 1', function(t) {
+    t.plan(1);
+
+    messengers[1].once('data', function(data) {
+      t.equal(data, 'hello');
+    });
+
+    messengers[0].send('hello');
+  });
+
+  test('1 --> 0', function(t) {
+    t.plan(1);
+
+    messengers[0].once('data', function(data) {
+      t.equal(data, 'hello');
+    });
+
+    messengers[1].send('hello');
+  });
+
+  test('close 1', function(t) {
+    t.plan(1);
+    messengers[1].close();
+    t.pass('closed');
+  });
+
+  test('0 --> 1 fails', function(t) {
+    t.plan(1);
+
+    messengers[1].once('data', function(data) {
+      t.fail('should not have received data');
+    });
+
+    setTimeout(function() {
+      t.pass('message was not received');
+    }, 100);
+
+    messengers[0].send('hello');
+  });
 };
